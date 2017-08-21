d33ac367c29711d9581a5de3572fbc813cf80d66
richseviora <richard.seviora@gmail.com>
2017-08-21 12:22:44 -0700

    Updated URL Generator to Support Generic Client Requests

--- src/client/URLGenerator.ts
+++ src/client/URLGenerator.ts
@@ -1,6 +1,7 @@
 import { IWarshipRequest } from "../api/requests/IWarshipRequest";
 import { URL, URLSearchParams } from "url";
-import {ServerRealm} from "../api/ServerRealm";
+import { ServerRealm } from "../api/ServerRealm";
+import { IClientRequest } from "../api/requests/IClientRequest";
 
 export interface IURLGenerator {
     warshipRequest(config?: Partial<IWarshipRequest>): URL;
@@ -16,7 +17,17 @@ export class URLGenerator implements IURLGenerator {
     }
 
     public warshipRequest(config: Partial<IWarshipRequest>): URL {
-        const url = new URL("ships/", this.baseUrl());
+        return this.clientRequest("ships/", config);
+    }
+
+    /**
+     * Generates the URL for the desired endpoint and request objects.
+     * @param {string} endPoint - Endpoint. Must end with a '/'.
+     * @param {Partial<T extends IClientRequest>} config - Configuration object for the request.
+     * @returns {"url".URL} The URL for the request.
+     */
+    public clientRequest<T extends IClientRequest>(endPoint: string, config: Partial<T>): URL {
+        const url = new URL(endPoint, this.baseUrl());
         const options = Object.assign({}, config, {
             application_id: this.applicationId
         });
@@ -26,6 +37,6 @@ export class URLGenerator implements IURLGenerator {
     }
 
     private baseUrl(): URL {
-      return new URL(`http://api.worldofwarships.${this.realm}/wows/encyclopedia/`);
+        return new URL(`http://api.worldofwarships.${this.realm}/wows/encyclopedia/`);
     }
 }
